9b88eeeb7d9628f156b9a26d9f5656cb518e7717
Alexander Korobov <dktwho@mail.ru>
2022-08-23 12:04:59 +0300

    delete AddTodo and add lazy

---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,41 @@
+import { func } from "prop-types";
+import React, {useState} from "react";
+import PropTypes  from 'prop-types';
+
+
+function useInputValue(defaultValue = '') {
+  const [value, setValue] = useState(defaultValue)
+
+  return {
+    bind: {
+      value,
+      onChange: event => setValue(event.target.value)
+    },
+   clear: () => setValue(''),
+   value: () => value
+  }
+}
+function AddTodo({onCreate}) {
+  const input = useInputValue('')
+
+  function submitHadler(event) {
+    event.preventDefault()
+    if(input.value().trim()) {
+      onCreate(input.value)
+      input.clear()
+      // setValue('')
+    }
+  }
+return(
+  <form style={{marginBottom: '1rem'}} onSubmit={submitHadler}>
+    <input {...input.bind}  />
+    <button type="submit">Add todo</button>
+  </form>
+)
+}
+
+AddTodo.propTypes = {
+  onCreate: PropTypes.func.isRequired
+}
+
+export default AddTodo
